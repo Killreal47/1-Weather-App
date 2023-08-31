78fb13bad50ea511316032ea1fd5fe2356d682ec
Kirill Rusinov <kirill.rusinov190820@mail.ru>
2023-08-31 10:34:16 +0300

    Пофиксил баги

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -1,19 +1,23 @@
 // https://home.openweathermap.org/api_keys
 
 function main() {
-	const apiKey = 'd73a2eedc036da5c15f1c6287e5a00c8',
-		apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+	const apiKey = 'd73a2eedc036da5c15f1c6287e5a00c8';
+	const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 	const searchBox = document.querySelector('.search input');
 	const searchBtn = document.querySelector('.search button');
-	const wetherIcon = document.querySelector('.weather-icons');
+	const weatherIcon = document.querySelector('.weather-icons');
+	const weather = document.querySelector('.weather');
 
 	async function checkWeather(city) {
 		const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-		if (response.status == 404 ) {
+		document.querySelector('.error').style.display = 'none';
+		document.querySelector('.error-null').style.display = 'none';
+
+		if (response.status == 404) {
 			document.querySelector('.error').style.display = 'block';
 			document.querySelector('.weather').style.display = 'none';
-		} else if(response.status == 404){
+		} else if (response.status == 400) {
 			document.querySelector('.error-null').style.display = 'block';
 		} else {
 			let data = await response.json();
@@ -24,27 +28,27 @@ function main() {
 			document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
 
 			if (data.weather[0].main == 'Clouds') {
-				wetherIcon.src = './img/icons/clouds.png';
+				weather.insertAdjacentHTML('afterbegin', '<img src="./img/icons/clouds.webp" class="weather-icons" alt="погода">' );
 			} else if (data.weather[0].main == 'Clear') {
-				wetherIcon.src = './img/icons/clear.png';
+				weather.insertAdjacentHTML('afterbegin', '<img src="./img/icons/clear.webp" class="weather-icons" alt="погода">' );
 			} else if (data.weather[0].main == 'Rain') {
-				wetherIcon.src = './img/icons/rain.png';
+				weather.insertAdjacentHTML('afterbegin', '<img src="./img/icons/rain.webp" class="weather-icons" alt="погода">' );
 			} else if (data.weather[0].main == 'Drizzle') {
-				wetherIcon.src = './img/icons/drizzle.png';
+				weather.insertAdjacentHTML('afterbegin', '<img src="./img/icons/drizzle.webp" class="weather-icons" alt="погода">' );
 			} else if (data.weather[0].main == 'Mist') {
-				wetherIcon.src = './img/icons/mist.png';
+				weather.insertAdjacentHTML('afterbegin', '<img src="./img/icons/mist.webp" class="weather-icons" alt="погода">' );
 			}
 
 			document.querySelector('.weather').style.display = 'block';
 			document.querySelector('.error').style.display = 'none';
+			document.querySelector('.error-null').style.display = 'none';
 
 		}
-
-
 	}
-
-
 	searchBtn.addEventListener('click', () => {
+		if(document.querySelector('.weather-icons')){
+			document.querySelector('.weather-icons').remove();
+		}
 		checkWeather(searchBox.value);
 		searchBox.value = '';
 		searchBox.focus();
@@ -52,26 +56,14 @@ function main() {
 
 	document.addEventListener('keydown', e => {
 		if (e.code === 'Enter') {
+			if(document.querySelector('.weather-icons')){
+				document.querySelector('.weather-icons').remove();
+			}
 			checkWeather(searchBox.value);
 			searchBox.value = '';
 			searchBox.focus();
 		}
 	});
-
-
-
-
-
-
-
-
-
-	document.addEventListener('keydown', (e) => {
-		if (e.code === "Escape" && modal.classList.contains('show')) {
-			closeModal();
-		}
-	});
-
 
 }
 
